a489dde6f747b6ee004a91d16e499de60aa3c5b1
Andy Casey <andycasey@gmail.com>
2018-10-19 08:31:22 +1100

    ci setup

---
+++ .ci/upload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+# Push to GitHub
+#cd $TRAVIS_BUILD_DIR
+#git checkout --orphan pdf
+#git rm -rf .
+#git add -f article/ms.pdf
+#git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
+#git push -q -f https://andycasey:$GITHUB_API_KEY@github.com/andycasey/mcfa pdf
